106099dcae3bbd8932fc01a6f5842ba1d9fc76ec
Kaher19 <kirbey.garcia19@gmail.com>
2022-08-19 19:50:10 -0500

    Cambio de nombre en la ruta

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/users', [UserController::class, 'index']);
+Route::get('/usuarios', [UserController::class, 'index']);
